b2e8cd841bcb96a150f3e38f0ed5f10654cc4923
joaobr200 <joaoarantesjob@gmail.com>
2022-06-30 00:05:29 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), babel()],
   build: {
     outDir: "dist",
     manifest: true,
   },
+  publicDir: "./public",
 });
